51212fdbd205b9b6de9c7559a89cf7bce1b7323c
NSANZUMUHIRE FIDELE <fidelensanze100@gmail.com>
2025-08-23 15:59:41 +0200

    main

--- ibabi/settings.py
+++ ibabi/settings.py
@@ -172,8 +172,7 @@ CSRF_TRUSTED_ORIGINS = [
     #"CORS_ALLOWED_ORIGINS", "http://localhost:8081"
 #).split(",")
 
-# FRONTEND URL
-FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://localhost:3000", "https://ibabi.vercel.app")
+FRONTEND_URL = os.environ.get("FRONTEND_URL", "https://ibabi.vercel.app")
 
 # MISC
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
